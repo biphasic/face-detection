157db94dc405e661159f33914d56dc4b183ae389
Gregor Lenz <lenz.gregor@gmail.com>
2018-05-29 17:30:54 +0200

    ported c++ matlab print

--- decay.m
+++ decay.m
@@ -1,10 +1,10 @@
-function events = decay(recording, resolution)
+function events = decay(recording, scaleFactorTime)
 %DECAY Summary of this function goes here
 %   Detailed explanation goes here
 
 timeconst = 50000;
-arrayLength = round((recording.ts(end)-recording.ts(1))/resolution);
-%if mod(recording.ts(end), resolution) ~= 0
+arrayLength = round((recording.ts(end)-recording.ts(1))/scaleFactorTime);
+%if mod(recording.ts(end), scaleFactorTime) ~= 0
 %    arrayLength = arrayLength + 1;
 %end
 
@@ -12,28 +12,42 @@ events.ts = zeros(1, arrayLength);
 events.activityOn = zeros(1, arrayLength);
 events.activityOff = zeros(1, arrayLength);
 
-%recording doesn't always start at 1
-for index = 1:arrayLength
-    timestamp = recording.ts(1)/resolution + resolution * (index -1);
-    events.ts(index) = timestamp;
-    
-    temp = recording.ts - timestamp;
-    diff = max(temp(temp<=0)); %difference to the previous timestamp
+l = length(recording.ts);
+lastScaledTS = 0;
+index = 1;
+%factor = exp(-scaleFactorTime/timeconst);
+factor = 1;
+starttime = floor(recording.ts(1)/scaleFactorTime);
 
-    if diff == 0 %accounts also for multiple events at one timestamp
-        tic
-        if ismember(0, recording.p(recording.ts == timestamp)) % there are OFF events for this timestamp
-            events.activityOn(index) = max(recording.activityOn((recording.ts == timestamp)));
+for event = 1:l
+    rec_ts = floor(recording.ts(event)/scaleFactorTime);
+    scaledTS = rec_ts - mod(rec_ts - starttime, scaleFactorTime);
+    regularTS = starttime + scaleFactorTime * (index - 1);
+    events.ts(index) = regularTS;
+    
+    valOn = events.activityOn(index);
+    valOff = events.activityOff(index);
+    
+    while regularTS < scaledTS
+        valOn = valOn * factor;
+        valOff = valOff * factor;
+        index = index + 1;
+        regularTS = regularTS + scaleFactorTime;
+        events.activityOn(index) = valOn;
+        events.activityOff(index) = valOff;
+        events.ts(index) = regularTS;
+    end
+    
+    if scaledTS ~= regularTS
+        index = index + 1;
+    end
+    if recording.p(event) == 1
+        events.activityOn(index) = recording.activityOn(event);
+        if index > 1
+            events.activityOff(index) = events.activityOff(index-1) * factor;
         end
-        toc
-        if ismember(1, recording.p(recording.ts == timestamp)) % there are ON events for this timestamp
-            events.activityOff(index) = max(recording.activityOff((recording.ts == timestamp)));
-        end            
-    %else
-    %    if recording.p(recording.ts == (events.ts(index) + diff)) == 1
-    %        events.activityOn(index) = recording.activityOn(recording.ts == (events.ts(index) + diff)) * exp(diff / timeconst)
-    %    else
-    %        events.activityOff(index) = recording.activityOff(recording.ts == (events.ts(index) + diff)) * exp(diff / timeconst)
-    %    end
+    else
+        events.activityOn(index) = valOn;
+        events.activityOff(index) = recording.activityOff(event);
     end
 end
