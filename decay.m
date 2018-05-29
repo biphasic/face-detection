637f5bc50a1f16405dffc0a57a1374786ef50422
Gregor Lenz <lenz.gregor@gmail.com>
2018-05-29 19:10:28 +0200

    decay factor in decay.m was missing a timescaleFactor

--- decay.m
+++ decay.m
@@ -3,20 +3,15 @@ function events = decay(recording, scaleFactorTime)
 %   Detailed explanation goes here
 
 timeconst = 50000;
-arrayLength = round((recording.ts(end)-recording.ts(1))/scaleFactorTime);
-%if mod(recording.ts(end), scaleFactorTime) ~= 0
-%    arrayLength = arrayLength + 1;
-%end
+arrayLength = floor((recording.ts(end)-recording.ts(1))/scaleFactorTime/scaleFactorTime);
 
-events.ts = zeros(1, arrayLength);
+events.ts = zeros(1, arrayLength+1);
 events.activityOn = zeros(1, arrayLength);
 events.activityOff = zeros(1, arrayLength);
 
 l = length(recording.ts);
-lastScaledTS = 0;
 index = 1;
-%factor = exp(-scaleFactorTime/timeconst);
-factor = 1;
+factor = exp(-(scaleFactorTime * scaleFactorTime)/timeconst);
 starttime = floor(recording.ts(1)/scaleFactorTime);
 
 for event = 1:l
@@ -31,23 +26,22 @@ for event = 1:l
     while regularTS < scaledTS
         valOn = valOn * factor;
         valOff = valOff * factor;
-        index = index + 1;
         regularTS = regularTS + scaleFactorTime;
         events.activityOn(index) = valOn;
         events.activityOff(index) = valOff;
         events.ts(index) = regularTS;
-    end
-    
-    if scaledTS ~= regularTS
         index = index + 1;
     end
+
     if recording.p(event) == 1
         events.activityOn(index) = recording.activityOn(event);
         if index > 1
             events.activityOff(index) = events.activityOff(index-1) * factor;
         end
     else
-        events.activityOn(index) = valOn;
+        if index > 1
+            events.activityOn(index) = events.activityOn(index-1) * factor;
+        end
         events.activityOff(index) = recording.activityOff(event);
     end
 end
