c36ca63b6092fa6e85fb60b8df4412c05ab8cc8d
Gregor Lenz <lenz.gregor@gmail.com>
2018-05-29 19:37:57 +0200

    printing 5 interpolated blinks over each other

---
+++ blink_extractor.m
@@ -0,0 +1,42 @@
+recording = fullfile('~', 'Recordings', 'face-detection', 'Fede', 'run1.es');
+
+if exist('events', 'var') == 0
+    tic
+    events = load_eventstream(recording);
+    toc
+end
+
+eye = crop_spatial(events, 142, 123, 19, 15);
+blinks = [2432000, 5129000, 6767000, 9021000, 10910000];
+colors = ["blue", "red",  "cyan", "magenta", "black"];
+blinkstart = 2002100;
+blinklength = 400000;
+eye = activity(eye, 50000, true);
+
+%representation = "events";
+representation = "continuous";
+
+if representation == "events"
+    for i = 1:length(blinks)
+        indexes = eye.ts >= blinks(i) & eye.ts <= (blinks(i)+blinklength);
+        on = stem(eye.ts(indexes)-blinks(i), eye.activityOn(indexes));
+        on.Color = colors(1);
+        hold on
+        off = stem(eye.ts(indexes)-blinks(i), eye.activityOff(indexes));
+        off.Color = colors(2);
+    end
+else
+    scaleFactor = 10;
+    eye = shannonise(eye, scaleFactor);
+    blinks = blinks / scaleFactor;
+    blinklength = blinklength / scaleFactor;
+    for i = 1:length(blinks)
+        indexes = eye.ts >= blinks(i) & eye.ts <= (blinks(i)+blinklength);
+        on = plot(eye.ts(indexes)-blinks(i), eye.activityOn(indexes));
+        on.Color = colors(1);
+        hold on
+        off = plot(eye.ts(indexes)-blinks(i), eye.activityOff(indexes));
+        off.Color = colors(2);
+    end
+end
+
