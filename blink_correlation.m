1449a1638512045e5f61e4ef6bd4a2e3f6c2a61b
Gregor Lenz <lenz.gregor@gmail.com>
2018-06-14 17:22:06 +0200

    print actual correlation instead of just indicators

--- blink_correlation.m
+++ blink_correlation.m
@@ -5,15 +5,13 @@
 %on.Color = 'blue';
 %off = stem(eye.ts, eye.activityOff)
 %off.Color = 'red';
-eye.PMindicators = nan(1, length(eye.ts));
+eye.PatternCorrelation = nan(1, length(eye.ts));
 eye.NumberOfEvents = nan(1, length(eye.ts));
 
 slidingWindowWidth = 400000;
 bufferScale = 100;
 bufferSize = slidingWindowWidth/bufferScale;
-minimumDifference = slidingWindowWidth/2;
-index = 1;
-newIndex = 1;
+minimumDifference = slidingWindowWidth/4;
 lastTS = 0;
 lastPM = 0;
 buffer = circVBuf(int64(bufferSize), int64(1), 0);
@@ -40,31 +38,31 @@ for i = 1:length(eye.ts)
     
     numberOfEventsWithinWindow = buffer.raw(buffer.fst:buffer.lst);
     numberOfEventsWithinWindow = length(numberOfEventsWithinWindow(numberOfEventsWithinWindow > 0));
-    if eye.ts(i) - lastPM >= minimumDifference && numberOfEventsWithinWindow > 30 && numberOfEventsWithinWindow < 300
+    if eye.ts(i) - lastPM >= minimumDifference && numberOfEventsWithinWindow > 60 && numberOfEventsWithinWindow < 300
+        buf = buffer.raw(buffer.fst:buffer.lst)' / amplitudeScale;
+        if length(buf) < bufferSize
+            zeroedBuf = zeros(1,bufferSize);
+            zeroedBuf(end-length(buf)+1:end) = buf;
+            buf = zeroedBuf;
+        end
+        samples = filteredAverageOn .* (buf>0);
+        res = xcorr(buf, samples, 'coeff');
         lastPM = eye.ts(i);
-        eye.PMindicators(i) = 1;
-
+        eye.PatternCorrelation(i) = res(bufferSize);
         eye.NumberOfEvents(i) = numberOfEventsWithinWindow;
     end 
 end
 
-windows = eye.ts(eye.PMindicators==1);
+windows = eye.ts(~isnan(eye.PatternCorrelation));
 disp('number of windows: ')
 length(windows)
 stem(eye.ts, eye.activityOn/amplitudeScale);
 hold on
 stem(eye.ts, eye.activityOff/amplitudeScale);
-odd = 1;
-for i=eye.ts(eye.PMindicators==1)
+for i=eye.ts(~isnan(eye.PatternCorrelation))
     temp = eye.NumberOfEvents(eye.ts == i)/100;
     plot([i i], [0 temp(~isnan(temp))]);
     stem(i, temp(~isnan(temp)));
-    odd = odd + 1;
-    odd = mod(odd, 2);
-    if odd
-        a = area([i-slidingWindowWidth i], [1 1]);
-    else
-        a = area([i-slidingWindowWidth i], [1.1 1.1]);
-    end
+    a = area([i-slidingWindowWidth i], [eye.PatternCorrelation(eye.ts == i) eye.PatternCorrelation(eye.ts == i)]);
     a.FaceAlpha = 0.3;
 end
