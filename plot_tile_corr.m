ce86f34a2c6cf7b96fb5757ed59de84ec3486b84
Gregor Lenz <mail@lenzgregor.com>
2018-09-26 11:37:00 +0200

    update plot_tile_corr script to use new OOP way of doing things

--- plot_tile_corr.m
+++ plot_tile_corr.m
@@ -1,33 +1,27 @@
-rec = eventsLaure;
-amplitudeScale = 73;
-i = 5;
-j = 8;
-filterOn = filteredAverageOn;
-filterOff = filteredAverageOff;
+s = 1;
+r = 2;
+rec = subjects(s).Recordings{r}.Eventstream;
+i = 9;
+j = 10;
+
+modelBlink = subjects(s).Modelblink;
 tile_width = 19;
 tile_height = 15;
-eye = crop_spatial(rec, (i-1) * tile_width, (j-1) * tile_height, tile_width, tile_height);
-eye = activity(eye, 50000, true);
-%tic
-%profile on
-eye = quick_correlation(eye, filterOn, filterOff, amplitudeScale);
-%profile viewer
-%toc
 
-slidingWindowWidth = 300000;
+eye = quick_correlation(subjects(s).Recordings{r}.Grids{1}{i,j}, modelBlink.AverageOn, modelBlink.AverageOff, subjects(s).AmplitudeScale, subjects(s).BlinkLength);
 
 correlationThreshold = 0.88;
 
 figure 
-stem(eye.ts, eye.activityOn/amplitudeScale);
+stem(eye.ts, eye.activityOn/subjects(s).AmplitudeScale);
 hold on;
-stem(eye.ts, -eye.activityOff/amplitudeScale);
+stem(eye.ts, -eye.activityOff/subjects(s).AmplitudeScale);
 plot([0 eye.ts(end)], [correlationThreshold correlationThreshold]);
 
 windows = eye.ts(~isnan(eye.patternCorrelation));
 disp(['Number of windows: ', num2str(length(windows))])
 for i=eye.ts(~isnan(eye.patternCorrelation))
-    a = area([i-slidingWindowWidth i], [eye.patternCorrelation(eye.ts == i) eye.patternCorrelation(eye.ts == i)]);
+    a = area([i-subjects.BlinkLength i], [eye.patternCorrelation(eye.ts == i) eye.patternCorrelation(eye.ts == i)]);
     a.FaceAlpha = 0.1;
     if eye.patternCorrelation(eye.ts == i) > correlationThreshold
         a.FaceColor = 'yellow';
