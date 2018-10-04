073bdc30401843af993087f5056b48c531e718f7
Gregor Lenz <mail@lenzgregor.com>
2018-10-04 22:26:39 +0200

    new locations and timings for laure and kevin

--- blink_extractor_outdoor.m
+++ blink_extractor_outdoor.m
@@ -2,54 +2,55 @@ figure
 hold on
 names = {'laure', 'kevin', 'francesco'};
 
-for s = 1:3%numel(names)
+for s = 2:2%1:numel(names)
     if s == 1
-        disp('Laure/2-filtered.es')
+        disp('Laure/1-filtered.es')
         laure = Subject(names{s});
-        laure.addrecording(2, Recording(outdoorEventsLaure(2), true));
-        laure.Recordings{2}.Center.Location = [136 155]; %mitte des Auges 
-        laure.Recordings{2}.Left.Location =   [ 47 147];
-        laure.Recordings{2}.Right.Location =  [229 154];
-        laure.Recordings{2}.Center.Times = [4136000, 5482000];
-        %laure.Recordings{2}.Left.Times = [14250000]; not a good shape
-        %laure.Recordings{2}.Right.Times = [28000000, 29200000];
-        laure.AmplitudeScale = 15;
-        laure.CorrelationThreshold = 0.9;
-        laure.addrecording(1, Recording(outdoorEventsLaure(1), false));
-        laure.addrecording(3, Recording(outdoorEventsLaure(3), false));
+        laure.addrecording(1, outdoorEventsLaure(1), true);
+        laure.Recordings{1}.Center.Location = [139 152];
+        laure.Recordings{1}.Left.Location =   [ 11 146];
+        laure.Recordings{1}.Right.Location =  [246 152];
+        laure.Recordings{1}.Center.Times = [646900, 7635260];
+        laure.Recordings{1}.Left.Times = [17337700];
+        laure.Recordings{1}.Right.Times = [27588050, 29688650];
+        laure.AmplitudeScale = 20;
+        laure.CorrelationThreshold = 0.86;
+        laure.addrecording(2, outdoorEventsLaure(2), false);
+        laure.addrecording(3, outdoorEventsLaure(3), false);
         outdoorSubjects(s) = laure;
     elseif s == 2
-        disp('Kevin')
+        disp('Kevin/2.es')
         kevin = Subject(names{s});
-        kevin.addrecording(2, Recording(outdoorEventsKevin(2), true));
-        kevin.Recordings{2}.Center.Location = [136 137]; %mitte des Auges
+        kevin.addrecording(2, outdoorEventsKevin(2), true);
+        kevin.Recordings{2}.Center.Location = [136 137];
         kevin.Recordings{2}.Left.Location =   [ 89 132];
         kevin.Recordings{2}.Right.Location =  [186 134];
-        kevin.Recordings{2}.Center.Times = [10440000, 18700000];
+        kevin.Recordings{2}.Center.Times = [10440000, 18697460];
         %kevin.Recordings{2}.Left.Times = [15470000, 17780000];
-        %kevin.Recordings{2}.Right.Times = [28000000, 29200000];
-        kevin.AmplitudeScale = 20;
-        kevin.CorrelationThreshold = 0.9;
-        kevin.addrecording(1, Recording(outdoorEventsKevin(1), false));
-        kevin.addrecording(3, Recording(outdoorEventsKevin(3), false));
+        kevin.Recordings{2}.Right.Times = [41417300, 46152760];
+        kevin.AmplitudeScale = 19;
+        kevin.CorrelationThreshold = 0.92;
+        kevin.addrecording(1, outdoorEventsKevin(1), false);
+        kevin.addrecording(3, outdoorEventsKevin(3), false);
         outdoorSubjects(s) = kevin;
     elseif s == 3
         disp('Francesco/1-filtered')
         francesco = Subject(names{s});
-        francesco.addrecording(1, Recording(outdoorEventsFrancesco(1), true));
+        francesco.addrecording(1, outdoorEventsFrancesco(1), true);
         francesco.Recordings{1}.Center.Location = [136 137];
         francesco.Recordings{1}.Left.Location = [47 135];
         francesco.Recordings{1}.Right.Location = [255 138];
         francesco.Recordings{1}.Center.Times = [8218000];%[1580000];
         francesco.Recordings{1}.Left.Times = [21615000];
         francesco.AmplitudeScale = 9;
-        francesco.addrecording(2, Recording(outdoorEventsFrancesco(2), false));
-        francesco.addrecording(3, Recording(outdoorEventsFrancesco(3), false));
+        francesco.CorrelationThreshold = 0.9;
+        francesco.addrecording(2, outdoorEventsFrancesco(2), false);
+        francesco.addrecording(3, outdoorEventsFrancesco(3), false);
         outdoorSubjects(s) = francesco;
     elseif s == 4
         disp('Gregor/test7-cour')
         gregor = Subject(names{s});
-        gregor.addrecording(1, Recording(eventsGregor, true));
+        gregor.addrecording(1, eventsGregor, true);
         gregor.Recordings{1}.Center.Location = [157 156];
         gregor.Recordings{1}.Right.Location = [223 148];
         gregor.Recordings{1}.Left.Location = [81 158];
@@ -60,23 +61,18 @@ for s = 1:3%numel(names)
     else
     end
     
-    %check for first recording that is training recording to calculate ModelBlink from
-    for r = 1:numel(outdoorSubjects(s).Recordings)
-        if ~isempty(outdoorSubjects(s).Recordings{r}) && outdoorSubjects(s).Recordings{r}.IsTrainingRecording
-            break
-        end
-    end
+    r = outdoorSubjects(s).gettrainingrecordingindex;
     
     %retrieve smoothed Model and its variance
     m = outdoorSubjects(s).Modelblink;
-    [m.AverageOn, m.AverageOff, m.VarianceOn, m.VarianceOff] = outdoorSubjects(s).Recordings{r}.getmodelblink(outdoorSubjects(s).AmplitudeScale, outdoorSubjects(s).BlinkLength, 30);
+    [m.AverageOn, m.AverageOff, m.VarianceOn, m.VarianceOff] = outdoorSubjects(s).Recordings{r}.getmodelblink(30);
     outdoorSubjects(s).Modelblink = m;
     ax = subplot(1,numel(names),s);
     %ax = subplot(1,1,1);
     hold on
     
     if 1 == 1
-        [blinksOn, blinksOff] = outdoorSubjects(s).Recordings{r}.getblinks(outdoorSubjects(s).AmplitudeScale, outdoorSubjects(s).BlinkLength);
+        [blinksOn, blinksOff] = outdoorSubjects(s).Recordings{r}.getallblinks();
         fields = fieldnames(blinksOn);
         for j = 1:length(fields)
             for i = 1:size(blinksOn.(fields{j}),1)
