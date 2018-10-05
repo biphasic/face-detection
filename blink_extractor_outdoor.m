a1c168ceb7e611c0fe129bc6a58ec0489d2a84fc
Gregor Lenz <mail@lenzgregor.com>
2018-10-05 18:24:21 +0200

    now calling subjects by actual name rather than index number

--- blink_extractor_outdoor.m
+++ blink_extractor_outdoor.m
@@ -14,10 +14,10 @@ for s = 1:numel(names)
         laure.Recordings{1}.Left.Times = [17337700];
         laure.Recordings{1}.Right.Times = [27588050, 29688650];
         laure.AmplitudeScale = 20;
-        laure.CorrelationThreshold = 0.86;
+        laure.CorrelationThreshold = 0.88;
         laure.addrecording(2, outdoorEventsLaure(2), false);
         laure.addrecording(3, outdoorEventsLaure(3), false);
-        outdoorSubjects(s) = laure;
+        outdoorSubjects.(names{s}) = laure;
     elseif s == 2
         disp('Kevin/2.es')
         kevin = Subject(names{s});
@@ -29,24 +29,25 @@ for s = 1:numel(names)
         %kevin.Recordings{2}.Left.Times = [15470000, 17780000];
         kevin.Recordings{2}.Right.Times = [41417300, 46152760];
         kevin.AmplitudeScale = 19;
-        kevin.CorrelationThreshold = 0.92;
+        kevin.CorrelationThreshold = 0.90;
         kevin.addrecording(1, outdoorEventsKevin(1), false);
         kevin.addrecording(3, outdoorEventsKevin(3), false);
-        outdoorSubjects(s) = kevin;
+        outdoorSubjects.(names{s}) = kevin;
     elseif s == 3
         disp('Francesco/1-filtered')
         francesco = Subject(names{s});
         francesco.addrecording(1, outdoorEventsFrancesco(1), true);
         francesco.Recordings{1}.Center.Location = [136 137];
         francesco.Recordings{1}.Left.Location = [47 135];
-        francesco.Recordings{1}.Right.Location = [255 138];
-        francesco.Recordings{1}.Center.Times = [8218000];%[1580000];
+        francesco.Recordings{1}.Right.Location = [258 138];
+        francesco.Recordings{1}.Center.Times = [8202000, 1580000];
         francesco.Recordings{1}.Left.Times = [21615000];
-        francesco.AmplitudeScale = 9;
+        francesco.Recordings{1}.Right.Times = [41767600, 46037840, 47094080];
+        francesco.AmplitudeScale = 11;
         francesco.CorrelationThreshold = 0.9;
         francesco.addrecording(2, outdoorEventsFrancesco(2), false);
         francesco.addrecording(3, outdoorEventsFrancesco(3), false);
-        outdoorSubjects(s) = francesco;
+        outdoorSubjects.(names{s}) = francesco;
     elseif s == 4
         disp('Gregor/test7-cour')
         gregor = Subject(names{s});
@@ -57,24 +58,24 @@ for s = 1:numel(names)
         gregor.Recordings{1}.Center.Times = [7640000]; %, 8570000
         gregor.Recordings{1}.Left.Times = [13640000,14888000 ]; %
         gregor.AmplitudeScale = 25;
-        outdoorsubjects(s) = gregor;
+        outdoorsubjects.(s) = gregor;
     else
     end
     
-    r = outdoorSubjects(s).gettrainingrecordingindex;
+    r = outdoorSubjects.(names{s}).gettrainingrecordingindex;
     
     %retrieve smoothed Model and its variance
-    m = outdoorSubjects(s).Modelblink;
-    [m.AverageOn, m.AverageOff, m.VarianceOn, m.VarianceOff] = outdoorSubjects(s).Recordings{r}.getmodelblink(30);
-    outdoorSubjects(s).Modelblink = m;
+    m = outdoorSubjects.(names{s}).Modelblink;
+    [m.AverageOn, m.AverageOff, m.VarianceOn, m.VarianceOff] = outdoorSubjects.(names{s}).Recordings{r}.getmodelblink(30);
+    outdoorSubjects.(names{s}).Modelblink = m;
     ax = subplot(1,numel(names),s);
     %ax = subplot(1,1,1);
     hold on
     
-    if 1 == 1
-        [blinksOn, blinksOff] = outdoorSubjects(s).Recordings{r}.getallblinks();
+    if 1 == 2
+        [blinksOn, blinksOff] = outdoorSubjects.(names{s}).Recordings{r}.getallblinks();
         fields = fieldnames(blinksOn);
-        for j = 1:length(fields)
+        for j = 3:length(fields)
             for i = 1:size(blinksOn.(fields{j}),1)
                 a = plot(blinksOn.(fields{j})(i,:));
                 [xcOn, lagOn] = xcorr(blinksOn.(fields{j})(1,:), blinksOn.(fields{j})(i,:));
@@ -85,15 +86,15 @@ for s = 1:numel(names)
                 lagDiffOff = lagOff(indexOff);
                 lagDiff = 0.6*lagDiffOn + 0.4*lagDiffOff;
                 if abs(lagDiff) > 1 %resembles accuracy of lagdiff * 100us
-                    timestamp = outdoorSubjects(s).Recordings{r}.(fields{j}).Times(i)-lagDiff*100;
+                    timestamp = outdoorSubjects.(names{s}).Recordings{r}.(fields{j}).Times(i)-lagDiff*100;
                     disp(['suggested timestamp for blink ', int2str(i), ' at ', fields{j}, ' is: ', int2str(timestamp)])
                 end
                 a.Color = 'blue';
                 b = plot(blinksOff.(fields{j})(i,:));
                 b.Color = 'red';
             end
-            [averageOnFirst, averageOffFirst] = outdoorSubjects(s).Recordings{r}.(fields{1}).getaverages();
-            [averageOn, averageOff] = outdoorSubjects(s).Recordings{r}.(fields{j}).getaverages();
+            [averageOnFirst, averageOffFirst] = outdoorSubjects.(names{s}).Recordings{r}.(fields{1}).getaverages();
+            [averageOn, averageOff] = outdoorSubjects.(names{s}).Recordings{r}.(fields{j}).getaverages();
             [xcOn, lagOn] = xcorr(averageOnFirst, averageOn);
             [~, indexOn] = max(abs(xcOn));
             lagDiffOn = lagOn(indexOn);
@@ -113,6 +114,7 @@ for s = 1:numel(names)
     ax.mainLine.LineWidth = 3;
     ax = shadedErrorBar(1:length(m.AverageOff), m.AverageOff, m.VarianceOff, 'lineprops', '-r');
     ax.mainLine.LineWidth = 3;
+    ylim([0 inf])
     
     clear(names{s}, 'r', 'm', 'ax', 's')
 end
--- plot_tile_corr.m
+++ plot_tile_corr.m
@@ -1,26 +1,28 @@
 subjects = outdoorSubjects;
-s = 3;
-r = 1;
+s = 2;
+r = 2;
+
+names = {'laure', 'kevin', 'francesco'};
 
 offGrid = 1;
 
 if offGrid == 1
-    rec = subjects(s).Recordings{r}.Eventstream;
+    rec = subjects.(names{s}).Recordings{r}.Eventstream;
     tileWidth = 19;
     tileHeight = 15;
-    blinkCoordinates = subjects(s).Recordings{r}.Left.Location;
+    blinkCoordinates = subjects.(names{s}).Recordings{r}.Center.Location;
     eye = crop_spatial(rec, blinkCoordinates(1)-tileWidth/2, blinkCoordinates(2)-tileHeight/2, tileWidth, tileHeight);
     eye = activity(eye, 50000, true);
 else
     grid = 2;
     i = 7;
     j = 10;
-    eye = subjects(s).Recordings{r}.Grids{grid}{i,j};
+    eye = subjects.(names{s}).Recordings{r}.Grids{grid}{i,j};
 end
 
-modelBlink = subjects(s).Modelblink;
+modelBlink = subjects.(names{s}).Modelblink;
 
-eye = quick_correlation(eye, modelBlink.AverageOn, modelBlink.AverageOff, subjects(s).AmplitudeScale, subjects(s).BlinkLength);
+eye = quick_correlation(eye, modelBlink.AverageOn, modelBlink.AverageOff, subjects.(names{s}).AmplitudeScale, subjects.(names{s}).BlinkLength);
 timeScale = 10;
 continuum = shannonise(eye, timeScale);
 
@@ -31,17 +33,17 @@ plot([0 eye.ts(end)], [correlationThreshold correlationThreshold]);
 hold on;
 
 if 1 == 2
-    stem(eye.ts, eye.activityOn/subjects(s).AmplitudeScale);
-    stem(eye.ts, -eye.activityOff/subjects(s).AmplitudeScale);
+    stem(eye.ts, eye.activityOn/subjects.(names{s}).AmplitudeScale);
+    stem(eye.ts, -eye.activityOff/subjects.(names{s}).AmplitudeScale);
 else
-    plot(continuum.ts*timeScale, continuum.activityOn/subjects(s).AmplitudeScale);
-    plot(continuum.ts*timeScale, -continuum.activityOff/subjects(s).AmplitudeScale);
+    plot(continuum.ts*timeScale, continuum.activityOn/subjects.(names{s}).AmplitudeScale);
+    plot(continuum.ts*timeScale, continuum.activityOff/subjects.(names{s}).AmplitudeScale);
 end
 
 windows = eye.ts(~isnan(eye.patternCorrelation));
 disp(['Number of windows: ', num2str(length(windows))])
 for i=eye.ts(~isnan(eye.patternCorrelation))
-    a = area([i-subjects(s).BlinkLength i], [eye.patternCorrelation(eye.ts == i) eye.patternCorrelation(eye.ts == i)]);
+    a = area([i-subjects.(names{s}).BlinkLength i], [eye.patternCorrelation(eye.ts == i) eye.patternCorrelation(eye.ts == i)]);
     a.FaceAlpha = 0.1;
     if eye.patternCorrelation(eye.ts == i) > correlationThreshold
         a.FaceColor = 'yellow';
