da04608e2811c8a4f49fc88f9968f33f53bd745a
Gregor Lenz <mail@lenzgregor.com>
2018-10-05 18:26:03 +0200

    only detect synchronous blinks after 1s of recording

--- classes/Subject.m
+++ classes/Subject.m
@@ -126,7 +126,7 @@ classdef Subject < handle
                     x = (x1(1) + x2(1))/2;
                     y = grid1.y(grid1.ts == valid(e));
                     y = y(1);
-                    if (valid(e) - valid(e-1)) < maximumDifference && isequal(grid1.y(grid1.ts == valid(e)), grid1.y(grid1.ts == valid(e-1))) && abs(x1(1) - x2(1)) < 60
+                    if valid(e) > 1000000 && (valid(e) - valid(e-1)) < maximumDifference && isequal(grid1.y(grid1.ts == valid(e)), grid1.y(grid1.ts == valid(e-1))) && abs(x1(1) - x2(1)) < 60
                         scatter3(ax, x, -valid(e), y, 'red', 'diamond', 'filled')
                     end
                 end
@@ -139,7 +139,7 @@ classdef Subject < handle
                     x = (x1(1) + x2(1))/2;
                     y = grid2.y(grid2.ts == valid(e));
                     y = y(1);
-                    if (valid(e) - valid(e-1)) < maximumDifference && isequal(grid2.y(grid2.ts == valid(e)), grid2.y(grid2.ts == valid(e-1))) && abs(x1(1) - x2(1)) < 60
+                    if valid(e) > 1000000 && (valid(e) - valid(e-1)) < maximumDifference && isequal(grid2.y(grid2.ts == valid(e)), grid2.y(grid2.ts == valid(e-1))) && abs(x1(1) - x2(1)) < 60
                         scatter3(ax, x, -valid(e), y, 'red', 'diamond', 'filled')
                     end
                 end
