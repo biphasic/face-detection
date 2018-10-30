aac96d7b029cf256d8b13af3db71ccf6fb695961
Gregor Lenz <mail@lenzgregor.com>
2018-10-30 19:51:44 +0100

    adjust outdoor script

--- blink_extractor_outdoor.m
+++ blink_extractor_outdoor.m
@@ -2,11 +2,11 @@ if ~exist('outdoorEventsLaure', 'var')
     load('recordingsOutdoor.mat')
 end
 names = {'laure', 'kevin', 'francesco'};
-outdoor = Collection();
+outdoor = Collection('outdoor');
 for s = 1:numel(names)
     if s == 1
         disp('Laure/1-filtered.es')
-        laure = Subject(names{s}, 'outdoor');
+        laure = Subject(names{s}, outdoor);
         laure.addrecording(1, outdoorEventsLaure(1), true);
         laure.Recordings{1}.Center.Location = [139 152];
         laure.Recordings{1}.Left.Location =   [ 11 146];
@@ -14,7 +14,6 @@ for s = 1:numel(names)
         laure.Recordings{1}.Center.Times = [646900, 7635260];
         laure.Recordings{1}.Left.Times = 17337700;
         laure.Recordings{1}.Right.Times = [27588050, 29688650];
-        laure.AmplitudeScale = 20;
         laure.CorrelationThreshold = 0.88;
         laure.addrecording(2, outdoorEventsLaure(2), false);
         laure.addrecording(3, outdoorEventsLaure(3), false);
@@ -22,7 +21,7 @@ for s = 1:numel(names)
         outdoor.(names{s}) = laure;
     elseif s == 2
         disp('Kevin/2.es')
-        kevin = Subject(names{s}, 'outdoor');
+        kevin = Subject(names{s}, outdoor);
         kevin.addrecording(2, outdoorEventsKevin(2), true);
         kevin.Recordings{2}.Center.Location = [136 137];
         kevin.Recordings{2}.Left.Location =   [ 89 132];
@@ -30,7 +29,6 @@ for s = 1:numel(names)
         kevin.Recordings{2}.Center.Times = [10440000, 18697460];
         %kevin.Recordings{2}.Left.Times = [15470000, 17780000];
         kevin.Recordings{2}.Right.Times = [41417300, 46152760];
-        kevin.AmplitudeScale = 19;
         kevin.CorrelationThreshold = 0.90;
         kevin.addrecording(1, outdoorEventsKevin(1), false);
         kevin.addrecording(3, outdoorEventsKevin(3), false);
@@ -38,7 +36,7 @@ for s = 1:numel(names)
         outdoor.(names{s}) = kevin;
     elseif s == 3
         disp('Francesco/1-filtered')
-        francesco = Subject(names{s}, 'outdoor');
+        francesco = Subject(names{s}, outdoor);
         francesco.addrecording(1, outdoorEventsFrancesco(1), true);
         francesco.Recordings{1}.Center.Location = [136 137];
         francesco.Recordings{1}.Left.Location = [47 135];
@@ -46,7 +44,6 @@ for s = 1:numel(names)
         francesco.Recordings{1}.Center.Times = [8202000, 1580000];
         francesco.Recordings{1}.Left.Times = 21615000;
         francesco.Recordings{1}.Right.Times = [41767600, 46037840, 47094080];
-        francesco.AmplitudeScale = 11;
         francesco.CorrelationThreshold = 0.9;
         francesco.addrecording(2, outdoorEventsFrancesco(2), false);
         francesco.addrecording(3, outdoorEventsFrancesco(3), false);
