093a5e95a615e11184e1a2462896d1b44f80b1e1
Gregor Lenz <mail@lenzgregor.com>
2018-11-05 11:04:11 +0100

    refactor the way how indoor recordings are loaded

--- load_recordings_indoor.m
+++ load_recordings_indoor.m
@@ -1,18 +1,16 @@
 tic
-for n = 1:3
-    eventsFede(n) = load_eventstream(['~/Recordings/face-detection/indoor/fede/run',num2str(n),'.es']);
-    eventsAlex(n) = load_eventstream(['~/Recordings/face-detection/indoor/alex/run',num2str(n),'.es']);
-    eventsLaure(n) = load_eventstream(['~/Recordings/face-detection/indoor/laure/run',num2str(n),'.es']);
-    disp(['loaded recording number ', num2str(n), ' for all subjects'])
+names = {'fede', 'alex', 'laure', 'laurent', 'pe', 'mylene', 'gregor', 'suzon'};
+
+for n = 1:length(names)
+    name = names{n};
+    for r = 1:3
+        path = ['~/Recordings/face-detection/indoor/', name, '/run', num2str(r),'.es'];
+        if exist(path, 'file') == 2 && isfield(recordings.indoor, name) && isempty(recordings.indoor.(name)(r))
+            recordings.indoor.(name)(r) =  load_eventstream(path);
+        else 
+            continue;
+        end
+        disp(['loaded recording number ', num2str(r), ' for subject ', name])
+    end
 end
 toc
-
-%path = fullfile('~', 'Recordings', 'face-detection');
-%
-%path = fullfile(path, subject);
-%if ~exist('events', 'var') || ~exist('loadedSubject', 'var') || ~strcmp(loadedSubject, subject)
-%tic
-%events = load_eventstream(path);
-%toc
-%loadedSubject = subject;
-%end
