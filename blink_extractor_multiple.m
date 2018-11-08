49fef20037e783fe6a5151feb0b06b3adb00e71b
Gregor Lenz <mail@lenzgregor.com>
2018-11-08 11:52:26 +0100

    change times and scale for right/center subjects

--- blink_extractor_multiple.m
+++ blink_extractor_multiple.m
@@ -11,16 +11,17 @@ for s = 1:numel(names)
     if s == 1
         n = 3;
         subject.addrecording(n, multipleEvents(n), true);
-        %subject.Recordings{n}.Center.Location = [133 140];
-        subject.Recordings{n}.Center.Location = [51 131];
-        subject.Recordings{n}.Center.Times = [6275000 11230000 15000000];
+        subject.Recordings{n}.Center.Location = [133 140];
+        %subject.Recordings{n}.Center.Location = [51 131];
+        subject.Recordings{n}.Center.Times = [3936000 15150000];
+        subject.Recordings{n}.Center.AmplitudeScaleScale = 1.4;
         subject.Recordings{n}.Left.Location = [25 136];
-        subject.Recordings{n}.Left.Times = [11230000    14720540 15000000];
+        subject.Recordings{n}.Left.Times = [5977000  11230000    14720540 15000000];
         %subject.Recordings{n}.Left.AmplitudeScaleScale = 0.2;
         subject.Recordings{n}.Right.Location =  [228 136];
         subject.Recordings{n}.Right.Times = [19294785];
-        %subject.Recordings{n}.Right.AmplitudeScaleScale = 2;
-        subject.CorrelationThreshold = 0.91;
+        subject.Recordings{n}.Right.AmplitudeScaleScale = 1.8;
+        subject.CorrelationThreshold = 0.88;
         subject.addrecording(1, multipleEvents(1), false);
         subject.addrecording(2, multipleEvents(2), false);
     end
