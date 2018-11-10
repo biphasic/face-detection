7fde271be4bf3bf67718b3e6ce33208f3e73bf9a
Gregor Lenz <mail@lenzgregor.com>
2018-11-10 11:39:10 +0100

    adapt load recordings scripts for indoor and outdoor

--- load_recordings_indoor.m
+++ load_recordings_indoor.m
@@ -1,11 +1,11 @@
 tic
 names = {'fede', 'alex', 'laure', 'laurent', 'pe', 'mylene', 'gregor', 'suzon'};
-
+recordingsIndoor = [];
 for n = 1:length(names)
     name = names{n};
     for r = 1:3
-        path = ['~/Recordings/face-detection/indoor/', name, '/run', num2str(r),'.es'];
-        if exist(path, 'file') == 2 && isfield(recordingsIndoor, name) && isempty(recordingsIndoor.(name)(r))
+        path = ['~/Recordings/face-detection/indoor/', name, '/', num2str(r), '/run', num2str(r),'.es'];
+        if exist(path, 'file') == 2 %&& isempty(recordingsIndoor.(name)(r))
             recordingsIndoor.(name)(r) =  load_eventstream(path);
         else
             continue;
--- load_recordings_outdoor.m
+++ load_recordings_outdoor.m
@@ -1,8 +1,21 @@
 tic
-for n = 1:3
-    outdoorEventsLaure(n) = load_eventstream(['/home/gregorlenz/Recordings/face-detection/outdoor/laure/converted/',num2str(n),'-filtered.es']);
-    %outdoorEventsKevin(n) = load_eventstream(['/home/gregorlenz/Recordings/face-detection//outdoor/kevin/converted/',num2str(n),'.es']);
-    %outdoorEventsFrancesco(n) = load_eventstream(['/home/gregorlenz/Recordings/face-detection//outdoor/francesco/converted/',num2str(n),'-filtered.es']);
-    disp(['loaded recording number ', num2str(n), ' for all subjects'])
+names = {'laure', 'kevin', 'francesco', 'gregor'};
+recordingsOutdoor = [];
+for n = 1:length(names)
+    name = names{n};
+    for r = 1:3
+        if strcmp(name, 'kevin')
+            path = ['~/Recordings/face-detection/outdoor/', name, '/converted/', num2str(r),'.es'];
+        else
+            path = ['~/Recordings/face-detection/outdoor/', name, '/converted/', num2str(r),'-filtered.es'];
+        end
+        if exist(path, 'file') == 2 %&& isfield(recordingsOutdoor, name) && isempty(recordingsOutdoor.(name)(r))
+            recordingsOutdoor.(name)(r) =  load_eventstream(path);
+        else
+            continue;
+        end
+        disp(['loaded recording number ', num2str(r), ' for subject ', name])
+    end
 end
+clear('names', 'name', 'path', 'n', 'r');
 toc
