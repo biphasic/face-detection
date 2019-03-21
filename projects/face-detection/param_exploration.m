43ecc8024a9c21bc5e284932493e20d3de78ecc4
Gregor Lenz <mail@lenzgregor.com>
2019-03-22 00:26:22 +0100

    add script

---
+++ projects/face-detection/param_exploration.m
@@ -0,0 +1,19 @@
+error = 1;
+
+count = 0;
+for sigmaX = 3:1:13
+    for posInertia = 0.9:0.01:0.99
+        for minProb = 0.001:0.002:0.02
+            count = count + 1;
+            outdoor.ricardo.Recordings{1}.calculatetracking(sigmaX, round(sigmaX * 2/3), posInertia, minProb);
+            outdoor.ricardo.Recordings{1}.calculatetrackingerrorViolaJones;
+            if outdoor.ricardo.Recordings{1}.AverageTrackingError < error
+                error = outdoor.ricardo.Recordings{1}.AverageTrackingError;
+                disp(['SigmaX: ', sigmaX, ', positionInertia: ', posInertia, ' min Probability: ', minProb])
+            end
+        end
+    end
+end
+
+format shortg
+c = clock
